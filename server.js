e83adf99c1a7e2b5052c29df8ef9de5736e1c600
dev-azeem-s <azeemsdev@gmail.com>
2024-09-06 08:20:50 -0400

    Basic express server setup with 3 routes set-password store-public-key verify-signature
    Server listens on port 3000

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require("express");
+
+const port = 3000;
+const app = express();
+app.use(express.json());
+
+let storedPublicKey = null;
+let hashedPassword = null;
+
+
+app.post("/set-password", async (req, res) => {
+    const { username, publicKey } = req.body;
+    console.log(username, publicKey);
+    res.status(200).json({ message: 'Registered successfully!' });
+});
+
+app.post("/store-public-key", async (req, res) => {
+    const { username, publicKey } = req.body;
+    console.log(username, publicKey);
+    res.status(200).json({ message: 'Registered successfully!' });
+});
+
+
+app.post('/verify-signature', (req, res) => {
+    const { message, signature } = req.body;
+    console.log(message, signature);
+
+    res.status(200).json({ message: 'Signature is valid' });
+});
+
+
+app.listen(port, () => {
+    console.log(`Server listening on port ${port}`);
+});
